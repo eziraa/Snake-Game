a278f2d7e268e1469980f18f5be7fa22ab416809
eziraa <ezratgab@gmail.com>
2024-02-01 00:13:53 +0300

    make screen instance inside game board module

--- game_board.py
+++ game_board.py
@@ -1,8 +1,19 @@
 # Impoerting necessary module
 from snake import Snake
 from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.tracer(0)
+screen.title("Snake Game")
+screen.bgcolor("black")
 
 snake = Snake()
+is_game_on = True
 
+while is_game_on:
+    snake.moveSnake()
+    screen.update()
+    time.sleep(0.4)
 
 snake.screen.exitonclick()
