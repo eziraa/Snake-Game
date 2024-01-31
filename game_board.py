bb28fb425974c483f02fc7e7c7e2a1d4dc907a2c
eziraa <ezratgab@gmail.com>
2024-02-01 01:17:50 +0300

    making food go to new position when it ate bt snake

--- game_board.py
+++ game_board.py
@@ -1,4 +1,5 @@
 # Impoerting necessary module
+import random
 from snake import Snake
 from food import Food
 from turtle import Turtle, Screen
@@ -26,5 +27,9 @@ while is_game_on:
     snake.moveSnake()
     screen.update()
     time.sleep(0.14)
+    if snake.head.distance(food) <= 10:
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        food.goto(random_x, random_y)
 
 snake.screen.exitonclick()
