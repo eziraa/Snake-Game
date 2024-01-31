27d85d8ceadfe8d48d9b1beed325850e869fad73
eziraa <ezratgab@gmail.com>
2024-02-01 01:36:23 +0300

    Scoreboard class made to write the current sscore

--- score_board.py
+++ score_board.py
@@ -8,3 +8,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.pencolor("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center",
+                   font=("Arial", 15, "normal"))
+        self.hideturtle()
