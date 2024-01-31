a65d955adf74a29c2fd12a47a2694a0272cb8527
eziraa <ezratgab@gmail.com>
2024-01-31 23:36:36 +0300

    createSegments method created to create snake semgemnts

--- snake.py
+++ snake.py
@@ -6,4 +6,13 @@ POSITIONS = [(0,0),(0,-20),(0,-40)]
 class Snake():
     
     def __init__(self) -> None:
-        pass
+        self.segments = []
+
+    def  createSegments(self):
+
+        for position in POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
